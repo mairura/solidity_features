1210c64261dcdb89c6c07d8e626c7408a0d64996
ted <mairurat@gmail.com>
2022-08-04 17:15:23 +0300

    Constants vs Var

---
+++ Examples/Constants.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13; 
+21442 gas
+contract Constants {
+    address public constant MY_ADDRESS = 0x3db7B2413ae63E7e2D1a462119787C3aa3c0459e;
+    uint public constant MY_UINT = 123;
+}
+
+//23553 gas
+contract Var {
+    address public MY_ADDRESS = 0x3db7B2413ae63E7e2D1a462119787C3aa3c0459e;
+}
