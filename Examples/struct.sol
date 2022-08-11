db486fe7a08c9dd8f40d3105d62f2ae2d431b0f6
ted <mairurat@gmail.com>
2022-08-11 18:15:20 +0300

    Building struct in solidity

---
+++ Examples/struct.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13; //specifies the compiler
+
+//struct allow grouping of similar data together
+contract Structs {
+    struct Car {
+        string model;
+        uint year;
+        address owner;
+    }
+
+    //defining state variable
+    Car public car; 
+    Car[] public cars;
+    mapping(address => Car[]) public carsByOwner;
+
+    //Initializaton of struct 3 ways:
+    //1. We put parameters into parentesis
+    //2. Use key - value pairs
+
+    function example() external {
+        Car memory toyota = Car("Toyota", 1990, msg.sender);
+        Car memory lambo = Car({model: "Lambo", year: 1990, owner: msg.sender});
+        Car memory tesla;
+        tesla.model = "Tesla";
+        tesla.year = 1990;
+        tesla.owner = msg.sender;
+
+        cars.push(toyota);
+        cars.push(lambo);
+        cars.push(tesla);
+    }
+
+}
