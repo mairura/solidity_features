671b1f4bab9a3a896e7cb1313e6e0e43eda2de84
ted <mairurat@gmail.com>
2022-08-13 14:50:55 +0300

    Solidity Events

---
+++ Examples/events.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13; //specifies the compiler
+
+contract Event {
+    event Log(string message, uint val);
+    event IndexedLog(address indexed sender, uint val);
+
+    function example() external {
+        emit Log("Foo", 1234);
+    }
+
+    event Message(address indexed _from, address indexed _to, string message);
+
+    function sendMessage(address _to, string calldata message) external {
+        emit Message(msg.sender, _to, message);
+    }
+}
